7f55de3d032866f15186e2e3c324a42e11e8bd6f
rajon6007 <paulrajon412@gmail.com>
2024-10-04 00:52:26 +0600

    forget password/reset password page design

---
+++ app/src/main/java/com/mrpaul/digitalkazi/New_Password.kt
@@ -0,0 +1,15 @@
+package com.mrpaul.digitalkazi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class New_Password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_password)
+
+    }
+}
